321b2022a62c718340c192933f8b44e0df13a46f
CodeJoshCode <simmonsj711@dupage.edu>
2023-02-18 15:09:19 -0600

    initial gradle project

---
+++ backend/src/main/java/com/cheesybaby/lrnr2/Lrnr2Application.java
@@ -0,0 +1,13 @@
+package com.cheesybaby.lrnr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lrnr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lrnr2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cheesybaby/lrnr2/Lrnr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheesybaby.lrnr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lrnr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
