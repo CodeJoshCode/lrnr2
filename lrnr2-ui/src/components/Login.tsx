aa9540b83953e9a4c97f899a0646f03bd2f55f00
CodeJoshCode <simmonsj711@dupage.edu>
2023-02-25 10:13:28 -0600

    initial ui commit

---
+++ lrnr2-ui/src/components/Login.tsx
@@ -0,0 +1,10 @@
+export default function Login () {
+  return (
+    <>
+      <div className='wrapper'>
+        <img src='../../public/lrnr2-logo.svg' alt='lrnr logo image' />
+        <p>enter - and then have caffeine</p>
+      </div>
+    </>
+  )
+}
---
+++ lrnr2-ui/src/components/core/TopNav.tsx
@@ -0,0 +1,24 @@
+import Breadcrumbs from "@mui/material/Breadcrumbs/Breadcrumbs";
+import Link from "@mui/material/Link/Link";
+import Typography from "@mui/material/Typography/Typography";
+
+export default function TopNav () {
+    <Breadcrumbs aria-label="breadcrumb">
+  <Link underline="hover" color="inherit" href="/">
+    MUI
+  </Link>
+  <Link
+    underline="hover"
+    color="inherit"
+    href="/material-ui/getting-started/installation/"
+  >
+    Core
+  </Link>
+  <Typography color="text.primary">Breadcrumbs</Typography>
+</Breadcrumbs>
+}
+
+interface TopNavProps {
+    linkList: Link[]
+
+}
